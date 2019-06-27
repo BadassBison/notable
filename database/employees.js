30492fccfcf885592189127103d04e39167b16dd
BadassBison <shawn.scott.xd@gmail.com>
2019-06-27 08:14:56 -0700

    Server config setup with employee & customer API routes

---
+++ database/employees.js
@@ -0,0 +1,20 @@
+const employees = [{
+    id: 1,
+    name: "Jack Dundy",
+    email: "jack@email.com",
+    position: "CEO"
+},
+{
+    id: 2,
+    name: "Bill Turner",
+    email: "bill@email.com",
+    position: "COO"
+},
+{
+    id: 3,
+    name: "Matt Kenneally",
+    email: "matt@email.com",
+    position: "CTO"
+}];
+
+module.exports = employees;
